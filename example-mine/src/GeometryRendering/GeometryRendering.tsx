8441a6f6b3c2247e28fb307ce7a5bdb5b622d32b
Obodo David <obododavid5@gmail.com>
2025-02-04 22:52:05 +0100

    abstract geometry rendering

---
+++ example-mine/src/GeometryRendering/GeometryRendering.tsx
@@ -0,0 +1,50 @@
+import { useRef, useEffect } from "react";
+import "@kitware/vtk.js/Rendering/Profiles/Geometry";
+import vtkFullScreenRenderWindow from "@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow";
+import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor";
+import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
+import vtkConeSource from "@kitware/vtk.js/Filters/Sources/ConeSource";
+import vtkOutlineFilter from "@kitware/vtk.js/Filters/General/OutlineFilter";
+
+function GeometryRendering() {
+	const context = useRef();
+	const vtkContainerRef = useRef(null);
+
+	useEffect(() => {
+		if (!context.current) {
+			const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+				rootContainer: vtkContainerRef.current,
+			});
+			const renderer = fullScreenRenderer.getRenderer();
+			const renderWindow = fullScreenRenderer.getRenderWindow();
+
+			// CONE
+			const coneSource = vtkConeSource.newInstance({ height: 1.0 });
+			const mapper = vtkMapper.newInstance();
+			const actor = vtkActor.newInstance();
+			mapper.setInputConnection(coneSource.getOutputPort());
+			actor.setMapper(mapper);
+			renderer.addActor(actor);
+			renderer.resetCamera();
+			renderWindow.render();
+
+			// FILTER
+			const filter = vtkOutlineFilter.newInstance();
+			filter.setInputConnection(coneSource.getOutputPort());
+			const outlineActor = vtkActor.newInstance();
+			const outlineMapper = vtkMapper.newInstance();
+			outlineActor.setMapper(outlineMapper);
+			outlineMapper.setInputConnection(filter.getOutputPort());
+			renderer.addActor(outlineActor);
+			renderWindow.render();
+		}
+	}, [vtkContainerRef]);
+
+	return (
+		<div>
+			<div ref={vtkContainerRef}></div>
+		</div>
+	);
+}
+
+export default GeometryRendering;
---
+++ example-mine/src/VolumeRendering/VolumeRendering.tsx
@@ -0,0 +1,3 @@
+export default function VolumeRendering() {
+	return <div></div>;
+}
